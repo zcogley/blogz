f356af9ee9e265e66743e155e078640caade4c0d
Zac Cogley <zaccogley@gmail.com>
2017-02-20 19:02:18 -0600

    inital commit

---
+++ models.py
@@ -0,0 +1,12 @@
+from google.appengine.ext import db
+
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    # TODO - we need an author field here; it should be required
